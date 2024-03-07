9ae512fa456409cc532dcea600426ca308a22c53
youngjoon.yoo <youngjoon.yoo@navercorp.com>
2024-03-07 12:35:17 +0900

    updating distace - image histogram

--- lecture1/utils.py
+++ lecture1/utils.py
@@ -3,14 +3,14 @@ import math
 
 
 def normalize_histogram(hist):
-    return hist / np.sum(np.sqrt(hist * hist))
+    return hist / np.sqrt(np.sum(hist * hist))
 
 def calculate_histogram_distance(hist1, hist2):
     """
     Calculate the Bhattacharyya distance between two histograms.
     """
     # Calculate the Bhattacharyya coefficient
-    bc = np.sum(np.sqrt(hist1 * hist2))
+    bc = np.sqrt(np.sum(hist1 * hist2))
     
     # Calculate the Bhattacharyya distance
     distance = -1*math.log(bc)
