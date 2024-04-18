3793073cd465a131adea264c9e1a8114c2543451
JongHan Leem <jleem42@gmail.com>
2024-04-19 00:01:22 +0900

    fix(lecture1): Fix Bhattacharyya distance calculation

--- lecture1/utils.py
+++ lecture1/utils.py
@@ -3,17 +3,17 @@ import math
 
 
 def normalize_histogram(hist):
-    return hist / np.sqrt(np.sum(hist * hist))
+    return hist / np.sum(hist)
+
 
 def calculate_histogram_distance(hist1, hist2):
     """
     Calculate the Bhattacharyya distance between two histograms.
     """
     # Calculate the Bhattacharyya coefficient
-    bc = np.sqrt(np.sum(hist1 * hist2))
-    
-    # Calculate the Bhattacharyya distance
-    distance = -1*math.log(bc)
-    
-    return distance
+    bc = np.sum(np.sqrt(hist1 * hist2))
 
+    # Calculate the Bhattacharyya distance
+    distance = -1 * math.log(bc)
+
+    return distance
